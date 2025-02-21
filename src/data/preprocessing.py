6e2ad97daabd60c55fffed6c502bc2c371b40551
Ntropy86 <kargeti@wisc.edu>
2025-02-21 12:54:18 -0600

    `👌improve:` Completed Data Preprocessing pipline and couple config changes

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,8 +1,8 @@
 from multiprocessing import process
-import os
+import sys
 import pandas as pd
 import numpy as np
-from pathlib import Path
+from numpy import dtype
 from typing import Dict, List, Tuple
 from datetime import datetime
 
@@ -31,31 +31,32 @@ class CrimeDataProcessor:
 
     Attributes:
     raw_data_path (str): Path for the raw data folder. Defaults to 'data/raw'
-    processed_data_path (str): Path of the folder for processed data to be stored. Defaults to 'data/processed' 
+    processed_data_path (str): Path of the folder for processed data to be stored. Defaults to 'data/processed'
+    start_ywar (int): Starting year for processing 
     expected_columns (dict): Key value pairs of the column names for chicago crimes dataset and it's expected datatype.
     """
 
-    def __init__(self,raw_data_path:str='data/raw', processed_data_path:str='data/processed') -> None:
+    def __init__(self,raw_data_path:str='data/raw', processed_data_path:str='data/processed', start_year:int=2001) -> None:
         self.raw_data_path = raw_data_path
         self.processed_data_path = processed_data_path
 
         # Define expected columns and their types
         self.expected_columns = {
-            'ID': 'str',
-            'Case Number': 'str',
-            'Date': 'datetime64[ns]',
-            'Block': 'str',
-            'Primary Type': 'category',
-            'Description': 'category',
-            'Location Description': 'category',
-            'Arrest': 'bool',
-            'Domestic': 'bool',
-            'Beat': 'str',
-            'District': 'str',
-            'Ward': 'str',
-            'Community Area': 'str',
-            'Latitude': 'float64',
-            'Longitude': 'float64'
+            'id': 'str',
+            'case_number': 'str',
+            'date': 'datetime64[ns]',
+            'block': 'str',
+            'primary_type': 'category',
+            'description': 'category',
+            'location_description': 'category',
+            'arrest': 'bool',
+            'domestic': 'bool',
+            'beat': 'str',
+            'district': 'str',
+            'ward': 'str',
+            'community_area': 'str',
+            'latitude': 'float64',
+            'longitude': 'float64'
         }
     
     def _load_raw_data(self,year) -> pd.DataFrame:
@@ -75,8 +76,9 @@ class CrimeDataProcessor:
             file_path = self.raw_data_path + '/' + f'chicago_crimes_{year}.csv'
             logger.info(f'Processing {file_path}')
 
-            df = pd.read_csv(file_path)
-            logger.debug(f'Sucessfully loaded {len(df)} for {year}')
+            df = pd.read_csv(file_path, low_memory=False)
+
+            logger.debug(f'Sucessfully loaded {len(df)} records for {year}')
             return df
         except Exception as e:
             logger.info(f'Error in Loading for {year} : {str(e)}')
@@ -102,8 +104,51 @@ class CrimeDataProcessor:
             logger.info('Starting Data Cleaning Process')
 
             # Analysis 
-            logger.debug(f'Df Description: {df.describe()}')
-            logger.debug(f'Df Info: {df.info()}')
+            # logger.debug(f'Df Description: {df.describe()}')
+            # logger.debug(f'Df Info: {df.info()}')
+
+            # df['Date'] = pd.to_datetime(df['Date'])
+            # df['Primary Type'] = df['Primary Type'].astype('category')
+            # df['Location Description'] = df['Location Description'].astype('category')
+            # df['Arrest'] = df['Arrest'].astype(bool)
+            # df['Domestic'] = df['Domestic'].astype(bool)
+
+            missing_cols = set(self.expected_columns.keys()) - set(df.columns)
+            
+            if missing_cols:
+                raise DataProcessingError(f'Missing a couple of Expected Columns. Are you sure you have the right Dataset? Missing {missing_cols}')
+
+            # For automated Conversion of Data Types Pandas requires this
+            dtype_mapping = {
+            'str': str,
+            'category': 'category',  # pandas understands 'category' directly
+            'bool': bool,
+            'float64': float,
+            'int64': int,
+            'datetime64[ns]': 'datetime64[ns]'
+        }
+                                    
+            for col, dtype_name in self.expected_columns.items():
+                if dtype_name == 'datetime64[ns]':
+                    df[col] = pd.to_datetime(df[col])
+                else:
+                    actual_type = dtype_mapping.get(dtype_name)
+                    if actual_type:
+                        df[col] = df[col].astype(actual_type)
+                    else:
+                        logger.warning(f"Unknown dtype {dtype_name} for column {col}")
+
+
+            
+            # Pandas 3.0 Future Warning Removal : DO NOT USE inplace as it is DEPRECATED 
+            df['latitude'] = df['latitude'].fillna(df['latitude'].mean())
+            df['longitude'] = df['longitude'].fillna(df['longitude'].mean())
+
+            df = df.drop_duplicates(subset=['id'], keep='first')
+
+            df = df.drop(['x_coordinate','y_coordinate'], axis=1)
+
+            logger.debug(f'Cleaned {len(df)} records!')
 
             # change [date, updated on]  to datetime 
             # Including Non Nulls : 
@@ -113,21 +158,101 @@ class CrimeDataProcessor:
             # Removal : [x,y coords, updatedon?, location?]
             # Category cols?
 
-
-
             return df
         except Exception as e:
             logger.error(f'Error in Cleaning Data : {str(e)}')
             raise DataProcessingError(f'Error in Cleaning Data : {str(e)}')
         
+    def _engineer_features(self,df:pd.DataFrame) -> pd.DataFrame:
+        """
+        Creates new features from the existing dataset
+
+        Args:
+        df (pd.DataFrame): Dataframe containing cleaned data 
+
+        Returns:
+        pd.DataFrame: Dataframe with new features added
+
+        Raises:
+        DataProcessingError : Error Raised in Feature Engineering. See message for more details
+        """
+
+        logger.info('Starting Feature Engineering Process!')
+        try:
+            df['year'] = df['date'].dt.year
+            df['month'] = df['date'].dt.month
+            df['day'] = df['date'].dt.day
+            df['hour'] = df['date'].dt.hour
+            df['day_of_week'] = df['date'].dt.dayofweek
+
+            bins = [0,6,12,15,18,22,24]
+            labels = ['Night','Morning','Afternoon','Evening','LateEvening','Night'] # 2 Nights so that we wrap around the Night time (22 -> 06)
+            df['time_of_day'] = pd.cut(df['hour'],bins=bins,labels=labels, ordered=False) # Ordered False as we have a Wrap around 
+            # Time of day produces some null data points keep that in mind
+
+            df['has_location'] = df['location'].notna()
+            df = df.drop(['location'],axis=1)
+
+            df['block'] = df['block'].str[5:] # Removed any unecessary sensitive information that is of no use to us.
+
+            logger.debug(f'Feature Engineering Sucessfully Completed!')
+            return df
+        except Exception as e:
+            logger.error(f'Failed to Create new features. {str(e)}')
+            raise DataProcessingError(f'Failed to Create new features. {str(e)}')
+        
+    def _process_year(self, year:int) -> None:
+        """
+        Processes the Raw Dataset for the year provided.
+        - Cleans the Data
+        - Engineers Features
+        - Saves the processed data in self.processed_data_path folder
+
+        Args:
+        year (int): Year of the Data Report to be Processed
+
+        Returns:
+        None
+
+        Raises:
+        DataProcessingError : If any of the above methods fail, this raises an Exception.
+        """
+        try:
+            df = self._load_raw_data(year)
+            df = self._clean_data(df)
+            df = self._engineer_features(df)
+
+            output_path = self.processed_data_path + '/' + f'chicago_crimes_{year}_processed.csv'
+            df.to_csv(output_path,index=False)
+            logger.info(f'Sucessfully Processed {len(df)} records and saved as {output_path}')
+            del(df)
+
+        except Exception as e:
+            logger.error(f'MAJOR: Cannot Process Data. {str(e)}')
+            raise DataProcessingError
+
+        
 def main():
     """
     Main Execution function for Data Cleaning
+
+    Important:
+    Make sure to run this code as python -m src.data.preprocessing <start_year> otherwise defaults to 2001
     """
     try:
-        data_cleaner = CrimeDataProcessor(raw_data_path='data/raw',processed_data_path='data/processed')
-        df = data_cleaner._load_raw_data('2021') # Aribtary currently to understand the dataset in question.
-        df = data_cleaner._clean_data(df)
+        if len(sys.argv)>1:
+            start_year = int(sys.argv[1])
+        else:
+            start_year = 2001
+
+        logger.info(f'Starting Preprocessing of All Chicago Crimes Dataset. Start year is {start_year}')
+        preprocessor = CrimeDataProcessor(raw_data_path='data/raw',processed_data_path='data/processed',start_year=start_year)
+        current_year = datetime.now().year
+
+        for year in range(start_year,current_year+1):
+            preprocessor._process_year(year)
+        
+
     except Exception as e:
         logger.error(f'Error in Running Application. {str(e)}')
         raise DataProcessingError(f'Error in Running Application. {str(e)}')
--- src/utils/logger_config.py
+++ src/utils/logger_config.py
@@ -21,17 +21,17 @@ def setup_logger(name):
 
     # Configs for how logs will appear in logs/
     file_format = logging.Formatter(
-        '%(levelname)s : %(name)s : %(funcName)s : %(lineno)d : %(message)s'
+        '%(levelname)s : %(name)s.%(funcName)s : %(message)s'
     )
 
-    log_file = f'logs/chicago_crime_{datetime.now().strftime("%m%d%Y")}'
+    log_file = f'logs/chicago_crime_{datetime.now().strftime("%m%d%Y")}.log'
     file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(file_format)
 
     # Console logs configs
     console_format = logging.Formatter(
-        '%(asctime)s - %(levelname)s - %(message)s'
+        '%(asctime)s - %(levelname)s : %(name)s.%(funcName)s : %(message)s'
     )
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
